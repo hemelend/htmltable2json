5aedd9dbeb432e121c36ff18c96368e6a7d67d9d
Hugo Melendez <humelend@microsoft.com>
2018-10-12 09:54:51 -0600

    added html parser library

--- HtmlTable2Json/Html2Json.cs
+++ HtmlTable2Json/Html2Json.cs
@@ -2,15 +2,17 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 
+
 namespace HtmlTable2Json
 {
-    public static class Function1
+    public static class Html2Json
     {
-        [FunctionName("Function1")]
+        [FunctionName("Html2Json")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
@@ -23,13 +25,20 @@ namespace HtmlTable2Json
             if (name == null)
             {
                 // Get request body
-                dynamic data = await req.Content.ReadAsAsync<object>();
-                name = data?.name;
+                dynamic data = req.Content.ReadAsStringAsync().Result;
+                name = data;
             }
 
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            var html = name;
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var htmltableBody = htmlDoc.DocumentNode.SelectSingleNode("//table");
+
+
+
+            //return req.CreateResponse(HttpStatusCode.OK, htmlBody.OuterHtml);
         }
     }
 }
