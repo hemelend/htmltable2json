ce072592959451550f5a7f05a553a2a350cc2709
Hugo Melendez <humelend@microsoft.com>
2018-10-12 19:02:42 -0600

    creating json object

--- HtmlTable2Json/Html2Json.cs
+++ HtmlTable2Json/Html2Json.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -6,7 +7,8 @@ using HtmlAgilityPack;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
-
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace HtmlTable2Json
 {
@@ -18,27 +20,48 @@ namespace HtmlTable2Json
             log.Info("C# HTTP trigger function processed a request.");
 
             // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+            string html = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "html", true) == 0)
                 .Value;
 
-            if (name == null)
+            if (html == null)
             {
                 // Get request body
                 dynamic data = req.Content.ReadAsStringAsync().Result;
-                name = data;
+                html = data;
             }
 
-            var html = name;
-
+            //ceate an Html Document object
             var htmlDoc = new HtmlDocument();
             htmlDoc.LoadHtml(html);
 
-            var htmltableBody = htmlDoc.DocumentNode.SelectSingleNode("//table");
+            //get the table tag content from Html document
+            var htmltableBody = htmlDoc.DocumentNode;
 
+            //generate a json object from html table
+            object json = Getjson(htmltableBody);
 
+            return req.CreateResponse(HttpStatusCode.OK, json);
+        }
 
-            //return req.CreateResponse(HttpStatusCode.OK, htmlBody.OuterHtml);
+        private static object Getjson(HtmlNode htmlNode)
+        {
+            // create a json object to store the data from htmlNode
+            var json = new JObject();
+
+            //recurring each element in HtmlNode and create a key/Value pairs arrays
+            foreach(var row in htmlNode.SelectNodes("//tr"))
+            {
+                // recurring the column in the table
+                if (row.HasChildNodes)
+                {
+                    var recs = row.SelectNodes("//td");
+                    // add json data from table td
+                    json[recs[0].InnerText.ToString()] = recs[1].InnerText.ToString();
+                }
+            }
+            // creating json object
+            return JsonConvert.SerializeObject(json);
         }
     }
 }
